5ca89f1a00a32a3b42643f4e22069cfac4359db1
Youssef658177 <josephsalex5@gmail.com>
2025-08-09 23:23:01 +0200

    Basic Data Types

---
+++ HackerRanck/C++/Beginner/Basic Data Types .cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include<iostream>
+
+ 
+#include <cmath>
+
+
+#include <stdio.h>
+
+
+#include<cstdio>
+
+
+using namespace std ;
+
+ 
+int main()
+{
+  
+
+ int a ;
+
+ 
+   long b ;
+
+ 
+   char c ;
+
+ 
+   float f ; 
+
+
+   double d ;
+
+ 
+   scanf ("%d %ld %c %f %lf", &a ,&b , &c, &f ,&d ) ;
+ 
+
+   printf ("%d",a);
+
+
+   printf ("%ld" ,b);
+
+
+   printf ("%c",c);
+
+ 
+  printf ("%f",f);
+
+
+   printf ("%lf",d);
+
+    
+return 0;
+}
